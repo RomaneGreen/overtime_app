8c8230c982a0f7ed2af08993420886a71bd4dc9a
Romane G <romane71193@gmail.com>
2019-02-22 22:36:59 -0500

    dry refactor

--- src/App.js
+++ src/App.js
@@ -1,91 +1,72 @@
-import React, { Component } from 'react';
-import './App.css';
-import video from './video.mp4';
-import watermark from './watermark.png';
+
+import React, {Component} from "react";
+import "./App.css";
+import video from "./video.mp4";
+import watermark from "./watermark.png";
+
+const width = 1280;
+const height = 720;
+const range = Array(5).fill().map((x, i) => <option key={i}>{i}</option>);
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
 
-componentDidMount() {
+    constructor() {
+        super();
+        this.state = {};
+    }
 
-	const canvas = document.createElement('canvas');
-	canvas.width = 1280;
-	canvas.height = 720;
-	const context = canvas.getContext('2d');
-	context.drawImage(document.querySelector('video'), 0, 0, 720, 1280);
-						context.drawImage(
-							document.querySelector('.watermark'),
-							parseInt(document.querySelector('select').value),
-							parseInt(document.querySelector('select').value)
-						);
-						if (document.querySelector('input[name=live]').checked) {
-							this.setState({ image: canvas.toDataURL() });
-						}				
-				
+    componentDidMount() {
+        this.canvasMagic(".watermark", "select", "select");
+    }
+
+    canvasMagic(el, x, y) {
+        const canvas = document.createElement("canvas");
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+        const context = canvas.getContext("2d");
+        context.drawImage(document.querySelector("video"), 0, 0, width, height);
+        context.drawImage(
+            document.querySelector(el),
+            parseInt(document.querySelector(x).value),
+            parseInt(document.querySelector(y).value)
+        );
+        if(document.querySelector("input[name=live]").checked) {
+            this.setState({image: canvas.toDataURL()});
+        }
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <video src={video} controls/>
+
+                <div>
+                    <div className="watermarkButton">
+
+                        <span className="watermarkButtonX">Watermark X
+                            <select className="positionX">{range}</select>
+                        </span>
+
+                        <span className="watermarkButtonY">Watermark Y
+                            <select className="positionY">{range}</select>
+                        </span>
+
+
+                        <span>Live</span>
+                        <input type="checkbox" name="live"/>
+                    </div>
+
+
+                    <button className="watermarkButton watermarkSubmit"
+                            onClick={() => this.canvasMagic(".watermark", ".positionX", ".positionY")}> Watermark!
+                    </button>
+
+                    <img alt="watermarks" className="watermark" src={watermark} style={{visibility: "hidden"}}/>
+                    <img alt="watermarks" className="imageDisplay" height="405px" width="560px" src={this.state.image}/>
+                </div>
+            </div>
+        );
+    }
 }
-
-
-	render() {
-		const range = [];
-		for (let i = 0; i < 1280; i++) {
-			range.push(i);
-		}
-
-		return (
-			<div className="app"> 
-				
-				<video	src={video} controls />
-
-				<div>
-				<div className="watermarkButton" style={{}}>
-
-					<span className="watermarkButtonX">Watermark X
-					<select className="positionX">
-						{range.map(i => (
-							<option key={i}>{i}</option>
-						))}
-					</select></span>
-
-					<span className="watermarkButtonY">Watermark Y
-					<select className="positionY">
-						{range.map(i => (
-							<option key={i}>{i}</option>
-						))}
-					</select></span>
-
-					
-
-					<span>Live</span>
-					<input type="checkbox" name="live" />
-					</div>
-					
-					
-					<button className = "watermarkButton watermarkSubmit"
-						onClick={() => {
-							const canvas = document.createElement('canvas');
-							canvas.width = 1280;
-							canvas.height = 720;
-							const context = canvas.getContext('2d');
-							context.drawImage(document.querySelector('video'), 0, 0, 1280, 720);
-							context.drawImage(
-								document.querySelector('.watermark'),
-								parseInt(document.querySelector('.positionX').value),
-								parseInt(document.querySelector('.positionY').value)
-							);
-							this.setState({ image: canvas.toDataURL() });
-						}}> Watermark! </button>
-
-
-					<img alt="watermarks" className="watermark" src={watermark} style={{ visibility: 'hidden' }} />
-					<img alt="watermarks" className="watermarkDisplay" height="405px" width="560px" src={this.state.image} />
-				</div>
-			</div>
-		);
-					}
-				
-				}
 
 export default App;
